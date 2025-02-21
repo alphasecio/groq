ca6d82f471a50f6fd12de0549e564e59c045347f
alphasec <95212748+alphasecio@users.noreply.github.com>
2025-02-21 12:04:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,76 +1,55 @@
 import os, streamlit as st
 from groq import Groq
 
-# Streamlit app
-st.subheader("Groq Playground")
+# Streamlit app config
+st.set_page_config(page_title="Groq Playground", initial_sidebar_state="auto")
+
+model_options = {
+    "Text: Meta Llama 3.3 70B": "llama-3.3-70b-versatile",
+    "Text: Google Gemma 2 9B Instruct": "gemma2-9b-it",
+    "Text: Mixtral 8x7B Instruct": "mixtral-8x7b-32768",
+    "Text: DeepSeek R1 (Preview)": "deepseek-r1-distill-llama-70b"
+}
+
+# Groq settings
 with st.sidebar:
-  groq_api_key = st.text_input("Groq API Key", type="password")
-  option = st.selectbox("Select Model", [
-    "Text: Meta Llama 3.3 70B",
-    "Text: Google Gemma 2 9B Instruct",
-    "Text: Mixtral 8x7B Instruct",
-    "Text: DeepSeek R1 (Preview)"]
-    )
+  st.subheader("Groq Playground")
+  groq_api_key = st.text_input("Groq API key", type="password")
+  model_option = st.selectbox("Select chat model", list(model_options.keys()))
+  model = model_options[model_option]
 
-prompt = st.text_input("Prompt", label_visibility="collapsed")
+# Initialise session state for client and messages
+if "messages" not in st.session_state:
+  st.session_state.messages = []
 
-os.environ["GROQ_API_KEY"] = groq_api_key
-client = Groq(api_key=groq_api_key)
+for message in st.session_state.messages:
+  with st.chat_message(message["role"]):
+    st.markdown(message["content"])
 
-# If Generate button is clicked
-if st.button("Generate"):
-  if not groq_api_key.strip() or not prompt.strip():
-    st.error("Please provide the missing fields.")
+# Initialize Groq client if key is provided
+if groq_api_key.strip():
+  if "client" not in st.session_state:
+    os.environ["GROQ_API_KEY"] = groq_api_key
+    st.session_state.client = Groq(api_key=groq_api_key)
+
+# User-Assistant chat interaction
+if prompt := st.chat_input("Ask anything"):
+  if not groq_api_key.strip():
+    st.error("Please provide Groq API key.")
+    st.stop()
   else:
     try:
-      with st.spinner("Please wait..."):
-        if option == "Text: Meta Llama 3.3 70B":
-          # Run llama-3.3-70b-versatile model on Groq
-          chat_completion = client.chat.completions.create(
-              messages=[
-                  {
-                      "role": "user",
-                      "content": prompt,
-                  }
-              ],
-              model="llama-3.3-70b-versatile",
-          )
-          st.success(chat_completion.choices[0].message.content)
-        elif option == "Text: Google Gemma 2 9B Instruct":
-          # Run gemma2-9b-it model on Groq
-          chat_completion = client.chat.completions.create(
-              messages=[
-                  {
-                      "role": "user",
-                      "content": prompt,
-                  }
-              ],
-              model="gemma2-9b-it",
-          )
-          st.success(chat_completion.choices[0].message.content)
-        elif option == "Text: Mixtral 8x7B Instruct":
-          # Run mixtral-8x7b-32768 model on Groq
-          chat_completion = client.chat.completions.create(
-              messages=[
-                  {
-                      "role": "user",
-                      "content": prompt,
-                  }
-              ],
-              model="mixtral-8x7b-32768",
-          )
-          st.success(chat_completion.choices[0].message.content)
-        elif option == "Text: DeepSeek R1 (Preview)":
-          # Run deepseek-r1-distill-llama-70b model on Groq
-          chat_completion = client.chat.completions.create(
-              messages=[
-                  {
-                      "role": "user",
-                      "content": prompt,
-                  }
-              ],
-              model="deepseek-r1-distill-llama-70b",
-          )
-          st.success(chat_completion.choices[0].message.content)
+      with st.chat_message("user"):
+        st.markdown(prompt)
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+      # Assistant response
+      with st.chat_message("assistant"):
+        response = st.session_state.client.chat.completions.create(
+            model=model,
+            messages=[{"role": "user", "content": prompt}]
+        )
+        st.write(response.choices[0].message.content)
+        st.session_state.messages.append({"role": "assistant", "content": response.choices[0].message.content})
     except Exception as e:
       st.exception(f"Exception: {e}")
